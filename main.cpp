ce44b135dd118b79f6c78353b178d5cd8246ba59
zhulinhui123 <2722772403@qq.com>
2024-07-26 10:41:18 +0800

    Second Update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <list>
 int main(int, char**){
+
     std::vector<std::string> strItem{"one", "two", "three", "four"};
-    for(std::string str : strItem)
+    std::map<int, std::string> intToStr;
+    intToStr.emplace(1, strItem[0]);
+    intToStr.emplace(2, strItem[1]);
+    intToStr.emplace(3, strItem[2]);
+    intToStr.emplace(4, strItem[3]);
+    intToStr[1];
+    for(auto it = intToStr.begin(); it != intToStr.end(); ++it)
     {
-        std::cout << str << std::endl;
+        std::cout << std::to_string(it->first) + ": " + it->second << std::endl;
+
     }
     return 0;
 }
